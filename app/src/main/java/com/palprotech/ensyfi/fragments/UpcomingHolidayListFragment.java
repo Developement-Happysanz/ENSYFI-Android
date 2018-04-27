8a87f1e86358e19201fd9c875be23f394226f7d7
Naren <naren.happysanz@gmail.com>
2018-04-27 16:18:52 +0530

    Holiday calendar finished

--- app/src/main/java/com/palprotech/ensyfi/fragments/UpcomingHolidayListFragment.java
+++ app/src/main/java/com/palprotech/ensyfi/fragments/UpcomingHolidayListFragment.java
@@ -1,5 +1,6 @@
 package com.palprotech.ensyfi.fragments;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -7,25 +8,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.palprotech.ensyfi.R;
 import com.palprotech.ensyfi.adapter.general.UpcomingHolidayListAdapter;
+import com.palprotech.ensyfi.bean.admin.support.StoreClass;
+import com.palprotech.ensyfi.bean.admin.support.StoreSection;
+import com.palprotech.ensyfi.bean.database.SQLiteHelper;
 import com.palprotech.ensyfi.bean.general.viewlist.UpcomingHoliday;
 import com.palprotech.ensyfi.bean.general.viewlist.UpcomingHolidayList;
 import com.palprotech.ensyfi.helper.AlertDialogHelper;
 import com.palprotech.ensyfi.helper.ProgressDialogHelper;
 import com.palprotech.ensyfi.servicehelpers.ServiceHelper;
 import com.palprotech.ensyfi.serviceinterfaces.IServiceListener;
+import com.palprotech.ensyfi.utils.CommonUtils;
 import com.palprotech.ensyfi.utils.EnsyfiConstants;
 import com.palprotech.ensyfi.utils.PreferenceStorage;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.Vector;
 
 public class UpcomingHolidayListFragment extends Fragment implements AdapterView.OnItemClickListener, IServiceListener {
 
@@ -42,6 +56,14 @@ public class UpcomingHolidayListFragment extends Fragment implements AdapterView
     int pageNumber = 0, totalCount = 0;
     String classId = "", sectionId = "", classSectionId = "", userType = "";
 
+    private Spinner spnClassList, spnSectionList, spnClassSecList;
+    RelativeLayout adminView, teacherView;
+    private String checkSpinner = "", storeClassId, storeSectionId;
+    SQLiteHelper db;
+    Vector<String> vecClassList, vecClassSectionList;
+    List<String> lsClassList = new ArrayList<String>();
+    String set3, AM_PM = "0";
+
     public UpcomingHolidayListFragment() {
     }
 
@@ -49,14 +71,10 @@ public class UpcomingHolidayListFragment extends Fragment implements AdapterView
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_upcoming_holiday, container, false);
+
         userType = PreferenceStorage.getUserType(getActivity());
-        Bundle bundle = getArguments();
-        if (userType.equals("1")||userType.equals("2")){
-            if(bundle != null) {
-                classId = getArguments().getString("class_id");
-                sectionId = getArguments().getString("section_id");
-                classSectionId = getArguments().getString("class_sec_id");
-            } else {
+        if (userType.equals("1") || userType.equals("2")) {
+            {
                 classId = "";
                 sectionId = "";
                 classSectionId = "";
@@ -66,27 +84,81 @@ public class UpcomingHolidayListFragment extends Fragment implements AdapterView
             sectionId = "";
             classSectionId = PreferenceStorage.getStudentClassIdPreference(getActivity());
         }
-        initializeViews();
         initializeEventHelpers();
+        initializeViews();
         return rootView;
     }
 
     protected void initializeViews() {
+
         Log.d(TAG, "initialize pull to refresh view");
         loadMoreListView = (ListView) rootView.findViewById(R.id.listView_holidays);
-       /* mNoEventsFound = (TextView) view.findViewById(R.id.no_home_events);
-        if (mNoEventsFound != null)
-            mNoEventsFound.setVisibility(View.GONE);
-        loadMoreListView.setOnLoadMoreListener(this); */
         loadMoreListView.setOnItemClickListener(this);
         upcomingHolidayArrayList = new ArrayList<>();
+        adminView = rootView.findViewById(R.id.admin_view_holiday);
+        teacherView = rootView.findViewById(R.id.teacher_view_holiday);
+        String userId = PreferenceStorage.getUserType(getActivity());
+        if (userId.equals("1")) {
+            adminView.setVisibility(View.VISIBLE);
+            GetClassData();
+            spnClassList = rootView.findViewById(R.id.class_list_spinner);
+            spnSectionList = rootView.findViewById(R.id.section_list_spinner);
+            spnClassList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    StoreClass classList = (StoreClass) parent.getSelectedItem();
+                    storeClassId = classList.getClassId();
+                    classId = storeClassId;
+                    GetSectionData();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                }
+            });
+
+            spnSectionList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    StoreSection sectionList = (StoreSection) parent.getSelectedItem();
+                    storeSectionId = sectionList.getSectionId();
+                    sectionId = storeSectionId;
+                    getHolsList();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                }
+            });
+        } else if (userId.equals("2")) {
+            db = new SQLiteHelper(getActivity());
+            vecClassList = new Vector<String>();
+            getClassSectionList();
+            teacherView.setVisibility(View.VISIBLE);
+            spnClassSecList = (Spinner) rootView.findViewById(R.id.class_section_list_spinner);
+            spnClassSecList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    String classSeactionName = parent.getItemAtPosition(position).toString();
+                    classSectionId = classSeactionName;
+                    getHolsList();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                }
+            });
+        } else {
+            teacherView.setVisibility(View.GONE);
+            adminView.setVisibility(View.GONE);
+            getHolsList();
+        }
     }
 
     protected void initializeEventHelpers() {
         serviceHelper = new ServiceHelper(getActivity());
         serviceHelper.setServiceListener(this);
         progressDialogHelper = new ProgressDialogHelper(getActivity());
-        getHolsList();
     }
 
     public void getHolsList() {
@@ -106,14 +178,170 @@ public class UpcomingHolidayListFragment extends Fragment implements AdapterView
         serviceHelper.makeGetServiceCall(jsonObject.toString(), url);
     }
 
+    private void GetClassData() {
+        checkSpinner = "class";
+        if (CommonUtils.isNetworkAvailable(getActivity())) {
+
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put(EnsyfiConstants.PARAMS_CLASS_ID, "1");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            progressDialogHelper.showProgressDialog(getString(R.string.progress_loading));
+            String url = EnsyfiConstants.BASE_URL + PreferenceStorage.getInstituteCode(getActivity()) + EnsyfiConstants.GET_CLASS_LISTS;
+            serviceHelper.makeGetServiceCall(jsonObject.toString(), url);
+
+
+        } else {
+            AlertDialogHelper.showSimpleAlertDialog(getActivity(), "No Network connection");
+        }
+    }
+
+    private void GetSectionData() {
+        checkSpinner = "section";
+        if (CommonUtils.isNetworkAvailable(getActivity())) {
+
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put(EnsyfiConstants.PARAMS_CLASS_ID_LIST, storeClassId);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            progressDialogHelper.showProgressDialog(getString(R.string.progress_loading));
+            String url = EnsyfiConstants.BASE_URL + PreferenceStorage.getInstituteCode(getActivity()) + EnsyfiConstants.GET_SECTION_LISTS;
+            serviceHelper.makeGetServiceCall(jsonObject.toString(), url);
+
+        } else {
+            AlertDialogHelper.showSimpleAlertDialog(getActivity(), "No Network connection");
+        }
+    }
+
+    private void getClassSectionList() {
+
+        try {
+            Cursor c = db.getTeachersClass();
+            if (c.getCount() > 0) {
+                if (c.moveToFirst()) {
+                    do {
+                        vecClassList.add(c.getString(1));
+                        set3 = c.getString(0);
+                    } while (c.moveToNext());
+                }
+            }
+            for (int i = 0; i < vecClassList.size(); i++) {
+                lsClassList.add(vecClassList.get(i));
+            }
+            HashSet hs = new HashSet();
+            TreeSet ts = new TreeSet(hs);
+            ts.addAll(lsClassList);
+            lsClassList.clear();
+//            lsStudent.add("Select");
+            lsClassList.addAll(ts);
+            db.close();
+            ArrayAdapter<String> dataAdapter3 = new ArrayAdapter<String>(getActivity(), R.layout.spinner_item_ns, lsClassList);
+//            adptStudent = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, lsStudent);
+//            adptStudent.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spnClassList.setAdapter(dataAdapter3);
+            spnClassList.setWillNotDraw(false);
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "Error", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
     }
 
+    private boolean validateSignInResponse(JSONObject response) {
+        boolean signInsuccess = false;
+        if ((response != null)) {
+            try {
+                String status = response.getString("status");
+                String msg = response.getString(EnsyfiConstants.PARAM_MESSAGE);
+                Log.d(TAG, "status val" + status + "msg" + msg);
+
+                if ((status != null)) {
+                    if (((status.equalsIgnoreCase("activationError")) || (status.equalsIgnoreCase("alreadyRegistered")) ||
+                            (status.equalsIgnoreCase("notRegistered")) || (status.equalsIgnoreCase("error")))) {
+                        signInsuccess = false;
+                        Log.d(TAG, "Show error dialog");
+                        AlertDialogHelper.showSimpleAlertDialog(getActivity(), msg);
+                    } else {
+                        signInsuccess = true;
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return signInsuccess;
+    }
+
     @Override
     public void onResponse(JSONObject response) {
-        LoadListView(response);
+
+        progressDialogHelper.hideProgressDialog();
+
+        if (validateSignInResponse(response)) {
+
+            try {
+                if (checkSpinner.equalsIgnoreCase("class")) {
+
+                    JSONArray getData = response.getJSONArray("data");
+                    JSONObject userData = getData.getJSONObject(0);
+                    int getLength = getData.length();
+                    Log.d(TAG, "userData dictionary" + userData.toString());
+
+                    String classId = "";
+                    String className = "";
+                    ArrayList<StoreClass> classesList = new ArrayList<>();
+
+                    for (int i = 0; i < getLength; i++) {
+
+                        classId = getData.getJSONObject(i).getString("class_id");
+                        className = getData.getJSONObject(i).getString("class_name");
+
+                        classesList.add(new StoreClass(classId, className));
+                    }
+
+                    //fill data in spinner
+                    ArrayAdapter<StoreClass> adapter = new ArrayAdapter<StoreClass>(getActivity(), R.layout.spinner_item_ns, classesList);
+                    spnClassList.setAdapter(adapter);
+                } else if (checkSpinner.equalsIgnoreCase("section")) {
+                    JSONArray getData = response.getJSONArray("data");
+                    JSONObject userData = getData.getJSONObject(0);
+                    int getLength = getData.length();
+                    Log.d(TAG, "userData dictionary" + userData.toString());
+
+                    String sectionId = "";
+                    String sectionclass = "";
+                    ArrayList<StoreSection> sectionList = new ArrayList<>();
+
+                    for (int i = 0; i < getLength; i++) {
+
+                        sectionId = getData.getJSONObject(i).getString("sec_id");
+                        sectionclass = getData.getJSONObject(i).getString("sec_name");
+
+                        sectionList.add(new StoreSection(sectionId, sectionclass));
+                    }
+
+                    //fill data in spinner
+                    ArrayAdapter<StoreSection> adapter = new ArrayAdapter<StoreSection>(getActivity(), R.layout.spinner_item_ns, sectionList);
+                    spnSectionList.setAdapter(adapter);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            LoadListView(response);
+        } else {
+            Log.d(TAG, "Error while sign In");
+        }
     }
 
     private void LoadListView(JSONObject response) {
@@ -133,7 +361,8 @@ public class UpcomingHolidayListFragment extends Fragment implements AdapterView
             totalCount = upcomingHolidayList.getCount();
             updateListAdapter(upcomingHolidayList.getUpcomingHolidays());
         }
-}
+    }
+
     protected void updateListAdapter(ArrayList<UpcomingHoliday> upcomingHolidayArrayList) {
         this.upcomingHolidayArrayList.addAll(upcomingHolidayArrayList);
        /* if (mNoEventsFound != null)
@@ -146,6 +375,7 @@ public class UpcomingHolidayListFragment extends Fragment implements AdapterView
             upcomingHolidayListAdapter.notifyDataSetChanged();
         }
     }
+
     @Override
     public void onError(String error) {
         progressDialogHelper.hideProgressDialog();
